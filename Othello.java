21b3b3d833c3dcd6c885a6dad1a5a8089c618732
Samuel Hagner <ens21ssg.cs@umu.se>
2025-09-16 23:41:35 +0200

    fixed for tests

--- Othello.java
+++ Othello.java
@@ -25,29 +25,56 @@ public class Othello{
 	 */
 	public static void main(String [] args){
 
-		String posString;
+        if (args.length < 2)
+		{
+            System.err.println("Error! This is too few arguments.");
+            System.exit(1);
+        }
+
+		String posString = args[0];
 		OthelloPosition pos;
 		OthelloAlgorithm algorithm;
 		OthelloAction move = null;
 
-		if(args.length > 0){
-			posString = args[0];
-		}else{
-			posString = "WEEEEEEEEEEEEEEEEEEEEEEEEEEEOXEEEEEEXOEEEEEEEEEEEEEEEEEEEEEEEEEEE";
+		if (posString.length() > 65) {
+			System.err.println("Error! String is to long! Should be 65 and got " + posString.length() + ".");
+			System.exit(1);
 		}
+		if (posString.length() < 65) {
+			System.err.println("Error! String is to short! Should be 65 and got " + posString.length() + ".");
+			System.exit(1);
+		}
+
+		Double timeLimitSeconds;
+		try {
+			timeLimitSeconds = Double.parseDouble(args[1]);
+		} catch (NumberFormatException e) {
+			System.err.println("Timelimit must be a number in seconds!");
+			System.exit(1);
+			return;
+		}
+		if (timeLimitSeconds <= 0) {
+			System.err.println("Time limit must be positive number!");
+			System.exit(1);
+		}
+
+		long timeLimitNanos = (long) (timeLimitSeconds * 1_000_000_000L);
+		long startTime = System.nanoTime();
+
+		// Iterative depth search with time limit.
+		int searchDepth = 1;
+
+		//if(args.length > 0){
+			//posString = args[0];
+		//}else{
+			//posString = "WEEEEEEEEEEEEEEEEEEEEEEEEEEEOXEEEEEEXOEEEEEEEEEEEEEEEEEEEEEEEEEEE";
+		//}
 		//System.out.println(posString);
 		pos = new OthelloPosition(posString);
 		//pos.illustrate(); //Only for debugging. The test script has it's own print method
 
 		// Which evaluator (heuristics) should be used
 		algorithm = new AlphaBeta(new EarlyGame());
-
-		// Iterative depth search with time limit.
-		int searchDepth = 1;
-
-		double timeLimitSeconds = Double.parseDouble(args[1]);
-		long timeLimitNanos = (long) (timeLimitSeconds * 1_000_000_000L);
-		long startTime = System.nanoTime();
 
 		while (true) {
 			long timeTaken = System.nanoTime() - startTime;
