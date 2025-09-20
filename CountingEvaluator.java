85893656cfd18e7c56c6e97dd36ce9d8571aaebd
Samuel Hagner <ens21ssg.cs@umu.se>
2025-09-20 22:08:53 +0200

    javadoc för countingelevator

--- CountingEvaluator.java
+++ CountingEvaluator.java
@@ -5,6 +5,16 @@
  */
 public class CountingEvaluator implements OthelloEvaluator {
 
+	/**
+	 * Calculates a score for a given position by counting squares for black and white player.
+	 *
+	 * It counts all white and black squares on the board and returns
+	 * whiteSquares - blackSquares. A higher value is good for White
+	 * and a negative value favors Black.
+	 *
+	 * @param pos, position to evaluate.
+	 * @return difference, whiteSquares - blackSquares.
+	 */
 	public int evaluate(OthelloPosition pos) {
 		OthelloPosition position = (OthelloPosition) pos;
 		int blackSquares = 0;
