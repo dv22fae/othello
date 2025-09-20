2d6da54b48486bea32c67bcc4ee4be2d7db4885b
Samuel Hagner <ens21ssg.cs@umu.se>
2025-09-20 22:35:02 +0200

    javadoc för timeexeption

--- TimeIsUpExeption.java
+++ TimeIsUpExeption.java
@@ -1,14 +1,16 @@
 /**
- * bapåfd påasd aposdkopw da
+ * Exception used to stop search when time limit is passed.
  *
  * @author Fredrik Alexandre, author
  * @author Samuel Hagner, author
- *
  * @version 1.0, 2025-09-24
  */
 
 public class TimeIsUpExeption extends RuntimeException {
 
+    /**
+     * Creates an exception saying that search is out of time.
+     */
     public TimeIsUpExeption() {
         super("Search time limit reached!");
     }
